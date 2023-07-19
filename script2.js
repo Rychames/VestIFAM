b0f41d6582536253d33a0c07a95379d248f3baf2
Rychames <122337054+Rychames@users.noreply.github.com>
2023-07-18 21:52:56 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,11 +1,25 @@
+var searchTerm = "";
+  var highlightedElements = [];
 
   function pesquisar() {
-    var searchTerm = document.getElementById("search-input").value.toLowerCase();
+    var newSearchTerm = document.getElementById("search-input").value.toLowerCase();
     var conteudo = document.getElementById("conteudo");
     var regex = new RegExp(searchTerm, "gi");
 
+    resetHighlight();
+
+    if (newSearchTerm.trim() === "") {
+      return;
+    }
+
+    searchTerm = newSearchTerm;
+
     var conteudoHTML = conteudo.innerHTML;
-    var conteudoDestacado = conteudoHTML.replace(regex, '<span class="highlighted">$&</span>');
+    var conteudoDestacado = conteudoHTML.replace(regex, function(matchedWord) {
+      var uniqueId = 'highlight-' + Math.random().toString(36).substr(2, 9);
+      highlightedElements.push(uniqueId);
+      return '<span id="' + uniqueId + '" class="highlighted">' + matchedWord + '</span>';
+    });
 
     if (conteudoHTML !== conteudoDestacado) {
       conteudo.innerHTML = conteudoDestacado;
@@ -20,3 +34,19 @@
     }
   }
 
+  function resetHighlight() {
+    var conteudo = document.getElementById("conteudo");
+
+    for (var i = 0; i < highlightedElements.length; i++) {
+      var element = document.getElementById(highlightedElements[i]);
+      if (element) {
+        element.outerHTML = element.innerHTML;
+      }
+    }
+    highlightedElements = [];
+  }
+
+  function limparPesquisa() {
+    document.getElementById("search-input").value = "";
+    resetHighlight();
+  }
