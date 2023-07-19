39cafab3ac0811547a024282f352ba10e942ec75
Rychames <122337054+Rychames@users.noreply.github.com>
2023-07-19 03:53:58 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,12 +1,28 @@
+function translateToEnglish() {
+    var textToTranslate = $('html').html(); // Obtém todo o conteúdo do site para tradução
 
-  function translateToEnglish() {
-    var elementsToTranslate = document.querySelectorAll('body *');
+    // Configurações da API de Tradução Texto do Azure
+    var subscriptionKey = 'fcae03746342418fb2d31b5eda939722';
+    var endpoint = 'https://api.cognitive.microsofttranslator.com/';
+    var location = 'brazilsouth';
 
-    var params = {
-      to: 'en'
-    };
+    var url = endpoint + 'translate?api-version=3.0&from=auto&to=en';
 
-    var translator = new Microsoft.Translator.Widget.TranslateWidget(params);
-    translator.showWidget(elementsToTranslate);
-  }
-
+    $.ajax({
+        url: url,
+        type: 'POST',
+        headers: {
+            'Ocp-Apim-Subscription-Key': subscriptionKey,
+            'Ocp-Apim-Subscription-Region': location,
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify([{ 'text': textToTranslate }]),
+        success: function (data) {
+            var translatedText = data[0].translations[0].text;
+            $('html').html(translatedText); // Atualiza o conteúdo traduzido no site
+        },
+        error: function (error) {
+            console.error('Erro na tradução:', error);
+        }
+    });
+}
