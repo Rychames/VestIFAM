ed61f0767886556172182810d23db013b61983e4
Rychames <122337054+Rychames@users.noreply.github.com>
2023-07-22 01:29:16 -0400

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,10 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+    const navItems = document.querySelector(".nav-items");
+
+    mobileMenuIcon.addEventListener("click", function () {
+      navItems.classList.toggle("show");
+    });
+  });
+
