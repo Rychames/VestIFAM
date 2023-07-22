c164980340184e15e19e255ce744442266d2526c
Rychames <122337054+Rychames@users.noreply.github.com>
2023-07-22 04:28:16 -0400

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
-    const navItems = document.querySelector(".nav-items");
 
-    mobileMenuIcon.addEventListener("click", function () {
-      navItems.classList.toggle("show");
-    });
-  });
