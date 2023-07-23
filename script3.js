4453214d5151814fcb24cf71f0a8b3c0f0a2cd58
Rychames <122337054+Rychames@users.noreply.github.com>
2023-07-22 20:01:33 -0400

    Add files via upload

--- script3.js
+++ script3.js
@@ -1,1 +1,39 @@
+// Inicialize a API Translator da Microsoft com sua chave (API key)
+const translatorConfig = {
+    apiKey: 'fcae03746342418fb2d31b5eda939722', // Substitua 'SUA_CHAVE_AQUI' pela sua chave de API da Microsoft Translator
+  };
+  const translator = new window.MicrosoftTranslator.Translator(translatorConfig);
 
+  // Função para traduzir para inglês
+  function translateToEnglish() {
+    translator.translateArray({
+      texts: ['.image-text'],
+      from: 'pt', // Português
+      to: 'en',  // Inglês
+    }, (err, result) => {
+      if (err) {
+        console.error('Erro ao traduzir para inglês:', err);
+        return;
+      }
+      // Atualizar o texto com a tradução
+      const englishText = result[0].translations[0].text;
+      document.querySelector('.image-text').innerText = englishText;
+    });
+  }
+
+  // Função para traduzir para português
+  function translateToPortuguese() {
+    translator.translateArray({
+      texts: ['.image-text'],
+      from: 'en', // Inglês
+      to: 'pt',  // Português
+    }, (err, result) => {
+      if (err) {
+        console.error('Erro ao traduzir para português:', err);
+        return;
+      }
+      // Atualizar o texto com a tradução
+      const portugueseText = result[0].translations[0].text;
+      document.querySelector('.image-text').innerText = portugueseText;
+    });
+  }
--- script6.js
+++ script6.js
@@ -1,14 +1,15 @@
-
-  // Função para abrir o modal
-  function openModal() {
+// Função para abrir o modal e desabilitar a rolagem da página
+function openModal() {
     var modal = document.getElementById("modal");
     modal.style.display = "block";
+    document.body.classList.add("modal-open");
   }
 
-  // Função para fechar o modal
+  // Função para fechar o modal e habilitar a rolagem da página novamente
   function closeModal() {
     var modal = document.getElementById("modal");
     modal.style.display = "none";
+    document.body.classList.remove("modal-open");
   }
 
   // Event listener para o botão de login no header
@@ -18,4 +19,3 @@
   // Event listener para o botão de login na sidebar
   var buttonLoginSidebar = document.getElementById("button-login-sidebar");
   buttonLoginSidebar.addEventListener("click", openModal);
-
