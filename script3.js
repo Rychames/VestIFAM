0c14b0305b2701507fb0ac2a856acb12509573bf
Rychames <122337054+Rychames@users.noreply.github.com>
2023-07-22 23:55:20 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,39 +1,47 @@
-// Inicialize a API Translator da Microsoft com sua chave (API key)
-const translatorConfig = {
-    apiKey: 'fcae03746342418fb2d31b5eda939722', // Substitua 'SUA_CHAVE_AQUI' pela sua chave de API da Microsoft Translator
-  };
-  const translator = new window.MicrosoftTranslator.Translator(translatorConfig);
+// Função para traduzir para inglês
+function translateToEnglish() {
+    const textToTranslate = document.querySelector('.image-text').innerText;
+    const apiKey = '6f4d0b92dd5940868af1cfed82ed7cd2'; // Substitua 'SUA_CHAVE_AQUI' pela sua chave de API da Microsoft Translator
 
-  // Função para traduzir para inglês
-  function translateToEnglish() {
-    translator.translateArray({
-      texts: ['.image-text'],
-      from: 'pt', // Português
-      to: 'en',  // Inglês
-    }, (err, result) => {
-      if (err) {
-        console.error('Erro ao traduzir para inglês:', err);
-        return;
-      }
-      // Atualizar o texto com a tradução
-      const englishText = result[0].translations[0].text;
-      document.querySelector('.image-text').innerText = englishText;
+    fetch(`https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=pt&to=en`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': apiKey,
+        'Ocp-Apim-Subscription-Region': 'brazilsouth', // Substitua pela região da sua chave de API
+      },
+      body: JSON.stringify([{ text: textToTranslate }]),
+    })
+    .then(response => response.json())
+    .then(data => {
+      const translatedText = data[0].translations[0].text;
+      document.querySelector('.image-text').innerText = translatedText;
+    })
+    .catch(error => {
+      console.error('Erro ao traduzir para inglês:', error);
     });
   }
 
   // Função para traduzir para português
   function translateToPortuguese() {
-    translator.translateArray({
-      texts: ['.image-text'],
-      from: 'en', // Inglês
-      to: 'pt',  // Português
-    }, (err, result) => {
-      if (err) {
-        console.error('Erro ao traduzir para português:', err);
-        return;
-      }
-      // Atualizar o texto com a tradução
-      const portugueseText = result[0].translations[0].text;
-      document.querySelector('.image-text').innerText = portugueseText;
+    const textToTranslate = document.querySelector('.image-text').innerText;
+    const apiKey = '6f4d0b92dd5940868af1cfed82ed7cd2s'; // Substitua 'SUA_CHAVE_AQUI' pela sua chave de API da Microsoft Translator
+
+    fetch(`https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en&to=pt`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': apiKey,
+        'Ocp-Apim-Subscription-Region': 'brazilsouth', // Substitua pela região da sua chave de API
+      },
+      body: JSON.stringify([{ text: textToTranslate }]),
+    })
+    .then(response => response.json())
+    .then(data => {
+      const translatedText = data[0].translations[0].text;
+      document.querySelector('.image-text').innerText = translatedText;
+    })
+    .catch(error => {
+      console.error('Erro ao traduzir para português:', error);
     });
   }
